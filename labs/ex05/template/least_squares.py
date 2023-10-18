6031047be043d3b1d417e1eedda4d70384511a4c
Vincent Roduit <101014161+roduit@users.noreply.github.com>
2023-10-18 14:48:04 +0200

    Final version of lab05

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -1,17 +1,47 @@
 # -*- coding: utf-8 -*-
-"""Exercise 3.
-
-Least Square
-"""
+"""a function used to compute the loss."""
 
 import numpy as np
 
+def compute_mse(y, tx, w):
+    """Calculate the loss using either MSE or MAE.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute loss by MSE
+    e = y - tx @ w
+    loss = 1/(2*len(y)) *np.linalg.norm(e)**2
+    return loss
+    # ***************************************************
 
 def least_squares(y, tx):
-    """calculate the least squares."""
+    """Calculate the least squares solution.
+       returns mse, and optimal weights.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar.
+
+    >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
+    (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
+    """
     # ***************************************************
     # INSERT YOUR CODE HERE
     # least squares: TODO
     # returns mse, and optimal weights
+    w = np.linalg.solve(tx.T@tx,tx.T@y)
+    loss = compute_mse(y,tx,w)
+    return w, loss
     # ***************************************************
-    raise NotImplementedError
